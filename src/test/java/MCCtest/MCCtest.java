6e4d256105830985d1f9d2b0eaeaeed6f60636f6
Tbult <55149824+Tbult@users.noreply.github.com>
2019-10-23 15:55:09 +0200

    Filling the input fields

--- src/test/java/MCCtest/MCCtest.java
+++ src/test/java/MCCtest/MCCtest.java
@@ -14,12 +14,29 @@ public class MCCtest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost:4200/app/research");
 //        driver.get("http://172.16.0.45/app#/research");
-//        driver.get("http://www.test.harpenslager.nl");
-//        driver.findElement(By.id("i_name")).sendKeys("Jansen");
+//        driver.findELement(By.id("")).sendKeys("");
         driver.findElement(By.xpath("/html/body/app-root/app-admin-layout/div/div[2]/div/app-research/div[2]/div/a")).click();
         System.out.println("Er is op knop onderzoek aanmaken geklikt");
-        driver.findElement(By.id("i_firstname")).sendKeys("henkie");
-//        class="btn btn-success btn-round"
+        // Onderzoek naam invoeren
+        driver.findElement(By.id("research-name")).sendKeys("Test Onderzoek");
+        // Naam eigenaar invoeren
+        driver.findElement(By.id("owner-name")).sendKeys("Schoon");
+        // Onderzoeksgebied invoeren
+        driver.findElement(By.id("research-area")).sendKeys("Amsterdamse Waterleidingduinen");
+        // Beschrijving invoeren
+        driver.findElement(By.id("description")).sendKeys("Dit is een automatische test");
+        // start datum invoeren
+        driver.findElement(By.id("start-date")).sendKeys("11/14/2019, 3:14 PM");
+        // eind datum invoeren
+        driver.findElement(By.id("end-date")).sendKeys("11/28/2019, 3:25 PM");
+        System.out.println("naam onderzoek, eigenaar, onderzoeksgebied, beschrijving, start datum en eind datum zijn ingevoerd");
+        // selecteer bron
+////        driver.findElement(By.xpath("/html/body/app-root/app-admin-layout/div/div[2]/div/app-research/div[2]/div/a")).click();
+//        driver.findElement(By.xpath("mat-select-2 > div:nth-child(1) > div:nth-child(1)")).click();
+////        driver.findElement(By.cssSelector("mat-option-9")).click();
+//        System.out.println("Eerste bron is geselecteerd");
 
+
+//        driver.findElement(By.xpath("")).click();
     }
     }
