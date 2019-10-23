38d6cf8b87f6bfd3eed75500abae680128756cc5
Tbult <55149824+Tbult@users.noreply.github.com>
2019-10-23 15:02:29 +0200

    Opening localhost and click the add research button in Selenium

--- src/test/java/MCCtest/MCCtest.java
+++ src/test/java/MCCtest/MCCtest.java
@@ -1,5 +1,6 @@
-//package MCCtest;
+package MCCtest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,9 +9,17 @@ import java.util.concurrent.TimeUnit;
 public class MCCtest {
     private static WebDriver driver = null;
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\t.bult.CIMSOLUTIONS\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://www.test.harpenslager.nl");
+        driver.get("http://localhost:4200/app/research");
+//        driver.get("http://172.16.0.45/app#/research");
+//        driver.get("http://www.test.harpenslager.nl");
+//        driver.findElement(By.id("i_name")).sendKeys("Jansen");
+        driver.findElement(By.xpath("/html/body/app-root/app-admin-layout/div/div[2]/div/app-research/div[2]/div/a")).click();
+        System.out.println("Er is op knop onderzoek aanmaken geklikt");
+        driver.findElement(By.id("i_firstname")).sendKeys("henkie");
+//        class="btn btn-success btn-round"
+
     }
     }
